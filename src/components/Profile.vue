9313f5007543e60becfbefe33369680e602d35a6
Yanina Trekhleb <yanisavchenko@gmail.com>
2020-11-28 20:52:11 +0100

    Add about section.

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,13 +1,49 @@
 <template>
-  <div id="profile">
-   <h2 class="">{{ title }}</h2>
-   <div class="avatar">
-     <p>HERE WILL BE IMAGE</p>
-     <img src="../images/avatar-image.jpg" alt="avatar-image">
-   </div>
-   <div class="about">
+  <div 
+    id="profile" 
+  >
+    <section
+      class="flex flex-row justify-center items-center "
+    >
+      <div class="avatar">
+        <img 
+          class="rounded-full h-72 w-72"
+          src="../images/avatar-image.jpg" 
+          alt="avatar-image"
+        >
+      </div>
+      <div class="about flex flex-col justify-center items-start">
+        <h2 
+          class="flex flex-row text-3xl text-center mb-1 leading-tight font-normal text-black"
+        >
+          {{ fullName }}
+        </h2>
+        <p>{{ position }}</p>
+        <p>&#128205;{{ location }}</p>
+        <div class="skills">
+          <span 
+            class="inline-block bg-gray-200 rounded-sm px-2 py-1 text-xs font-normal mr-2 mb-2 last:mr-0"
+          >
+            html/css
+          </span>
+          <span 
+            class="inline-block bg-gray-200 rounded-sm px-2 py-1 text-xs font-normal mr-2 mb-2 last:mr-0"
+          >
+            javascript
+          </span>
+          <span 
+            class="inline-block bg-gray-200 rounded-sm px-2 py-1 text-xs font-normal mr-2 mb-2 last:mr-0"
+          >
+            vue.js
+          </span>
+        </div>
+        <div
+          class="contacts"
+        >
 
-   </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -16,7 +52,9 @@
     name: 'Profile',
     data () {
       return {
-      title: 'Profile Title'
+      fullName: 'Yanina Trekhleb',
+      position: 'Junior Front-end Developer',
+      location: 'Amsterdam, The Netherlands'
       }
     }
   }
