238b0f12e09c3e85ad69d4866eb06f4dfef2d9fe
Yanina Trekhleb <yanisavchenko@gmail.com>
2021-10-22 19:13:48 +0200

    Add certificate.

--- src/database/certificates.js
+++ src/database/certificates.js
@@ -1,13 +1,22 @@
 export const certificates = [
   {
-    name: 'Animated Banner',
-    url: 'https://yaninatrekhleb.github.io/animated-banner-bootstrap/',
-    img: require('../images/projects-img/tulip-banner.png'),
-    date: 'Apr 2021 - Apr 2021',
+    name: 'How to Make a Website with NameCheap Course',
+    url: '',
+    img: require('../images/certificates-img/How-to-Make-a-Website-with-NameCheap-Course-Codecademy.jpg'),
+    date: 'Jan 2018 - Feb 2018',
     tags: [
-      { name: 'landing page' },
-      { name: 'bootstrap' },
       { name: 'html/css' },
+      { name: 'javascript' },
+    ],
+  },
+  {
+    name: 'HTML 5 App Development Fundamentals',
+    url: '',
+    img: require('../images/certificates-img/softserve-basic-frontend-course-y-trekhleb.png'),
+    date: 'Feb 2018 - Mar 2018',
+    tags: [
+      { name: 'html/css' },
+      { name: 'javascript' },
     ],
   },
 ]
