deca2572abe2075b3b41fc6dd2d278f95edd8f2e
Yanina Trekhleb <yanisavchenko@gmail.com>
2021-10-30 15:16:35 +0200

    Add certificates.

--- src/database/certificates.js
+++ src/database/certificates.js
@@ -1,9 +1,151 @@
 export const certificates = [
   {
-    name: 'How to Make a Website with NameCheap Course',
+    name: 'Foundations: Data, Data, Everywhere',
     url: '',
-    img: require('../images/certificates-img/How-to-Make-a-Website-with-NameCheap-Course-Codecademy.jpg'),
-    date: 'Jan 2018 - Feb 2018',
+    img: require('../images/certificates-img/Google-Data-Part1-Foundations-Data-Data-Everywhere.jpg'),
+    date: 'Oct 2021 - Oct 2021',
+    tags: [
+      { name: 'data' },
+      { name: 'data analysis' },
+    ],
+  },
+  {
+    name: 'Introduction to Python',
+    url: '',
+    img: require('../images/certificates-img/Introduction_to_Python_datacamp.jpg'),
+    date: 'Sep 2021 - Oct 2021',
+    tags: [
+      { name: 'python' },
+    ],
+  },
+  {
+    name: 'Google Tag Manager Fundamentals',
+    url: '',
+    img: require('../images/certificates-img/Google-Tag-Manager-Fundamentals-Course_Certificate.jpg'),
+    date: 'Mar 2021 - Mar 2021',
+    tags: [
+      { name: 'google analytics' },
+      { name: 'google ads' },
+      { name: 'google tag' },
+    ],
+  },
+  {
+    name: 'Advanced Google Analytics',
+    url: '',
+    img: require('../images/certificates-img/Advanced-Google-Analytics-Course_Certificate.jpg'),
+    date: 'Mar 2021 - Mar 2021',
+    tags: [
+      { name: 'google analytics' },
+      { name: 'google ads' },
+    ],
+  },
+  {
+    name: 'Google Analytics for Beginners',
+    url: '',
+    img: require('../images/certificates-img/Google-Analytics-for-Beginners-Course_Certificate_Yanina_Trekhleb.jpg'),
+    date: 'Mar 2021 - Mar 2021',
+    tags: [
+      { name: 'google analytics' },
+      { name: 'google ads' },
+    ],
+  },
+  {
+    name: 'Learn Vue.js Course',
+    url: '',
+    img: require('../images/certificates-img/Learn-Vue.js-Course.jpg'),
+    date: 'Jul 2020 - Aug 2020',
+    tags: [
+      { name: 'vue.js' },
+      { name: 'javascript' },
+    ],
+  },
+  {
+    name: 'Learn Vue.js Course',
+    url: '',
+    img: require('../images/certificates-img/Learn-Vue.js-Course.jpg'),
+    date: 'Jul 2020 - Aug 2020',
+    tags: [
+      { name: 'vue.js' },
+      { name: 'javascript' },
+    ],
+  },
+  {
+    name: 'Learn SQL Course',
+    url: '',
+    img: require('../images/certificates-img/Learn-SQL-Course.jpg'),
+    date: 'Jul 2020 - Jul 2020',
+    tags: [
+      { name: 'sql' },
+      { name: 'database' },
+    ],
+  },
+  {
+    name: 'Learn JavaScript Course',
+    url: '',
+    img: require('../images/certificates-img/Learn-JavaScript-Course-Codecademy.jpg'),
+    date: 'Jun 2020 - Jul 2020',
+    tags: [
+      { name: 'html/css' },
+      { name: 'javascript' },
+    ],
+  },
+  {
+    name: 'Learn React.js: Part I Course',
+    url: '',
+    img: require('../images/certificates-img/Learn-React.js-Part-I-Codecademy-react.jpg'),
+    date: 'Jun 2020 - Jul 2020',
+    tags: [
+      { name: 'html/css' },
+      { name: 'javascript' },
+      { name: 'react.js' },
+    ],
+  },
+  {
+    name: 'Building Interactive JavaScript Websites Course',
+    url: '',
+    img: require('../images/certificates-img/Building-Interactive-JavaScript-Websites-codecademy.jpg'),
+    date: 'Jan 2019 - Feb 2020',
+    tags: [
+      { name: 'html/css' },
+      { name: 'javascript' },
+    ],
+  },
+  {
+    name: 'Learn CSS Course',
+    url: '',
+    img: require('../images/certificates-img/learn-css-codecademy.jpg'),
+    date: 'Dec 2018 - Jan 2019',
+    tags: [
+      { name: 'html/css' },
+    ],
+  },
+  {
+    name: 'Learn Responsive Design Course',
+    url: '',
+    img: require('../images/certificates-img/Learn-Responsive-Design-Course-Codecademy-responsive.jpg'),
+    date: 'Dec 2018 - Jan 2019',
+    tags: [
+      { name: 'html/css' },
+      { name: 'responsive design' },
+      { name: 'javascript' },
+    ],
+  },
+  {
+    name: 'Responsive Web Design',
+    url: '',
+    img: require('../images/certificates-img/Responsive-Web Design-Coursera-2018.jpg'),
+    date: 'Sep 2018 - Oct 2018',
+    tags: [
+      { name: 'html/css' },
+      { name: 'responsive design' },
+      { name: 'javascript' },
+    ],
+  },
+  {
+    name: 'Responsive Website Basics: Code with HTML, CSS, and JavaScript',
+    url: '',
+    img: require('../images/certificates-img/Responsive-Website-Coursera-2018.jpg'),
+    date: 'Sep 2018 - Oct 2018',
     tags: [
       { name: 'html/css' },
       { name: 'javascript' },
@@ -14,6 +156,16 @@ export const certificates = [
     url: '',
     img: require('../images/certificates-img/softserve-basic-frontend-course-y-trekhleb.png'),
     date: 'Feb 2018 - Mar 2018',
+    tags: [
+      { name: 'html/css' },
+      { name: 'javascript' },
+    ],
+  },
+  {
+    name: 'How to Make a Website with NameCheap Course',
+    url: '',
+    img: require('../images/certificates-img/How-to-Make-a-Website-with-NameCheap-Course-Codecademy.jpg'),
+    date: 'Jan 2018 - Feb 2018',
     tags: [
       { name: 'html/css' },
       { name: 'javascript' },
