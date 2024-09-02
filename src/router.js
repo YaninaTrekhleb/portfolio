d199766cb87fdbebcb9d107d4705a2536bc14dd9
yanina <yanisavchenko@gmail.com>
2024-09-02 21:00:48 +0200

    Using Hash router

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Profile from './components/Profile.vue'
 import Projects from './components/Projects.vue'
 import Certificates from './components/Certificates.vue'
@@ -23,7 +23,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   base: process.env.BASE_URL,
   routes
 })
