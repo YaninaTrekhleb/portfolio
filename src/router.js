1a04ef6e2d8d61dd1e0aca09e02df818eaa0756c
yanina <yanisavchenko@gmail.com>
2024-09-02 20:56:37 +0200

    Update base URL

--- src/router.js
+++ src/router.js
@@ -24,7 +24,7 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  // base: process.env.BASE_URL,
+  base: process.env.BASE_URL,
   routes
 })
 
